2442eeb6b7bf99c863a179d9177a623ec8a83d42
Santosh kumar payasi <spayasi78@gmail.com>
2025-09-07 20:18:24 +0530

    feat: Implement new public layout and navigation, enhance homepage, and update global styles This commit introduces a new public-facing layout and navigation, improves the homepage experience, and refines global styling for better maintainability and appearance.

---
+++ app/(public)/_components/Navbar.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import { buttonVariants } from '@/components/ui/button'
+import { ThemeToggle } from '@/components/ui/theme-toggle'
+import { authClient } from '@/lib/auth-client'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import UserDropDown from './UserDropDown'
+
+interface NavigationDesktopItemsProps {
+    name: string,
+    href: string
+}
+
+
+const NavigationDesktopItems: NavigationDesktopItemsProps[] = [
+    {
+        name: "Home",
+        href: "/"
+    },
+    {
+        name: "Courses",
+        href: "/courses"
+    },
+    {
+        name: "Dashboard",
+        href: "/dashboard"
+    }
+]
+const Navbar = () => {
+    const { data: session, isPending } = authClient.useSession();
+    return (
+        <header className='sticky  top-0 z-50 w-full border-b bg-background/95 backdrop-blur-[backdrop-filter]:bg-background/60'>
+            <div className='container flex min-h-16 items-center mx-auto px-4 md:px-6 lg:px-8'>
+                <Link href={"/"} className='flex items-center space-x-2 mr-4'>
+                    <Image src={"/logo.png"} alt="Logo" width={32} height={32} />
+                    <span className='font-bold'>GyanGanga</span>
+                </Link>
+                <nav className='hidden md:flex  md:flex-1 md:items-center md:justify-between '>
+                    <div className='flex items-center space-x-2'>
+                        {
+                            NavigationDesktopItems.map((item) => (
+                                <Link
+                                    href={item.href}
+                                    key={item.href}
+                                    className='text-sm font-medium transition-colors hover:text-primary'
+                                >
+                                    {item.name}
+                                </Link>
+                            ))
+                        }
+                    </div>
+                    <div className='flex items-center space-x-4'>
+                        <ThemeToggle />
+
+                        {
+                            isPending ? null : session ? (
+                                <UserDropDown
+                                    name={session.user.name}
+                                    email={session.user.email}
+                                    image={session.user.image || ""}
+                                />
+                            ) : (
+                                <>
+                                    <Link href={"/login"} className={buttonVariants({ variant: "secondary" })}>
+                                        Login
+                                    </Link>
+                                    <Link href={"/"} className={buttonVariants({ variant: "default" })}>
+                                        GetStarted
+                                    </Link>
+                                </>
+                            )
+                        }
+                    </div>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
