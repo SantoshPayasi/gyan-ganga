592c9470fc3bbb7ffb1288f782e8e70a92aa4400
Santosh kumar payasi <spayasi78@gmail.com>
2025-09-24 15:44:56 +0530

    prefinal fixes

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,7 +3,7 @@ import { emailOTPClient, adminClient } from "better-auth/client/plugins"
 import { env } from "./env"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: env.BETTER_AUTH_URL,
+    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
     plugins: [
         emailOTPClient(),
         adminClient()
